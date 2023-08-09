2cbfeebdf3c1f3420ccc1421196cc2e32c9efa73
OGUNLEYE AYODAMOLA <ogunleyedamola1995@yahoo.com>
2023-08-09 15:47:39 +0100

    first-commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  const renderAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className='animal-list'>
+        {renderAnimals}
+      </div>
+    </div>
+  );
+}
+
+export default App;
